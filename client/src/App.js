b495f3ba6ebcf73c1fd7a785c10449b989d55e77
Neil Dhand <neil_dhand@baylor.edu>
2017-12-18 15:28:19 -0600

    chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Chart from './components/chart/Chart'
+
 
 const App = () =>
     <Router>
@@ -11,7 +13,7 @@ const App = () =>
             <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route component={NoMatch} />
-            </Switch>
+            </Switch>            
         </div>
     </Router>;
 
